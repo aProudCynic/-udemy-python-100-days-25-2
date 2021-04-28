13301e94190d066d9e576682e04122964bae0f9d
Balázs Márton <bmarton87@gmail.com>
2021-04-28 22:28:40 +0200

    feature: removed guessed state, display map

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pandas
+from turtle import Screen
 
 STATES_FILE_PATH = '50_states.csv'
+US_STATE_MAP_FILE_PATH = 'blank_states_img.gif'
 
 def load_states_from_file():
     return pandas.read_csv(STATES_FILE_PATH)
     
+screen = Screen()
+screen.bgpic(US_STATE_MAP_FILE_PATH)
+
 states_not_guessed = load_states_from_file()
 while len(states_not_guessed) > 0:
     guess = input(f'Guess a state ({len(states_not_guessed)} left): ')
     state_guessed = states_not_guessed[states_not_guessed['state'] == guess]
     if not state_guessed.empty:
         print(f'{guess} is correct')
+        states_not_guessed = states_not_guessed[states_not_guessed['state'] != guess]
     else:
         print(f'{guess} is incorrect, try again')
+screen.exitonclick()
