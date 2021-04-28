06545eab505329050674be4380817de4217e3283
Balázs Márton <bmarton87@gmail.com>
2021-04-28 23:25:28 +0200

    feature: switched to turtle input

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ screen = Screen()
 screen.bgpic(US_STATE_MAP_FILE_PATH)
 
 states_not_guessed = load_states_from_file()
+start_number_of_states = len(states_not_guessed)
 while len(states_not_guessed) > 0:
-    guess = input(f'Guess a state ({len(states_not_guessed)} left): ')
+    
+    guess = screen.textinput(f'{len(states_not_guessed)}/{start_number_of_states} left', f'Guess a state: ')
     state_guessed = states_not_guessed[states_not_guessed['state'] == guess]
     if not state_guessed.empty:
         print(f'{guess} is correct')
