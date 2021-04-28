23e28421936844207291a563ea65ff0d66147616
Balázs Márton <bmarton87@gmail.com>
2021-04-28 23:30:59 +0200

    feature: record correct guesses

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas
 from turtle import Screen
+
+from pandas.core.frame import DataFrame
 from state_display import StateDisplay
 
 STATES_FILE_PATH = '50_states.csv'
@@ -12,6 +14,7 @@ screen = Screen()
 screen.bgpic(US_STATE_MAP_FILE_PATH)
 
 states_not_guessed = load_states_from_file()
+correct_guesses = DataFrame()
 start_number_of_states = len(states_not_guessed)
 while len(states_not_guessed) > 0:
     
@@ -19,5 +22,6 @@ while len(states_not_guessed) > 0:
     state_guessed = states_not_guessed[states_not_guessed['state'] == guess]
     if not state_guessed.empty:
         states_not_guessed = states_not_guessed[states_not_guessed['state'] != guess]
+        correct_guesses.add(state_guessed)
         StateDisplay(state_guessed)
 screen.exitonclick()
